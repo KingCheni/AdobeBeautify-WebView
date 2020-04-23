d3738e91231d2a98c6ebbc07f008c7da9195217b
KingCheni <1141513431@qq.com>
2020-04-23 21:43:49 +0800

    updata

---
+++ js/function.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//     (function abUiInitialize() {
+//         $.ajax({
+//             url: '../config/setting/config.json',
+//             dataType: 'json',
+//             async: false,
+//             success: function(res) {
+//                 var config = res;
+//                 console.log(config);
+
+//             }
+//         });
+//     })()
+// })
